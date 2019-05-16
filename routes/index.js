b0f6fa472750b9c4d54388f584e4f965d408e631
SANDRA LERMA MARTIN <t151744@t0.inet>
2019-05-16 12:18:03 +0200

    Funciona!

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,16 @@ router.get('/', (req, res, next) => {
 router.post('/posts/:id/act', (req, res, next) => {
     const action = req.body.action;
     const counter = action === 'Like' ? 1 : -1;
-    Post.update({_id: req.params.id}, {$inc: {likes_count: counter}}, {}, (err, numberAffected) => {
+    Post.updateOne({_id: req.params.id}, {$inc: {likes_count: counter}}, {}, (err, numberAffected) => {
         let Pusher = require('pusher');
 
         let pusher = new Pusher({
-            appId: process.env.PUSHER_APP_ID,
-            key: process.env.PUSHER_APP_KEY,
-            secret: process.env.PUSHER_APP_SECRET,
+            appId: "782170",
+            key: "13c943dba30d6e63edb1",
+            secret: "98e3c6aebd88da1bceba",
             cluster: 'eu'
         });
-
+console.log( '???????????'+process.env.PUSHER_APP_ID);
         let payload = { action: action, postId: req.params.id };
         pusher.trigger('post-events', 'postAction', payload, req.body.socketId);
         res.send('');
